6cbd4b15c616e7de1aceb11181a6f84a289cd736
Johnny Hu <johnny@ciandt.com>
2019-12-09 11:31:28 -0800

    Update build

--- dist/index.js
+++ dist/index.js
@@ -7692,13 +7692,14 @@ const dangerfile_1 = __webpack_require__(640);
 try {
     // `who-to-greet` input defined in action metadata file
     const nameToGreet = core.getInput('who-to-greet');
+    const manualMergeTag = core.getInput('manual-merge-label');
     console.log(`Hello ${nameToGreet}!`);
     const time = new Date().toTimeString();
     core.setOutput('time', time);
     // Get the JSON webhook payload for the event that triggered the workflow
     const payload = JSON.stringify(github.context.payload, undefined, 2);
     console.log(`The event payload: ${payload}`);
-    new dangerfile_1.DangerChecks({ manualMergeTag: 'manual merge' }).run();
+    new dangerfile_1.DangerChecks({ manualMergeTag }).run();
 }
 catch (error) {
     core.setFailed(error.message);
