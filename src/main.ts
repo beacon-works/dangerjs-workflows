0a7c83d4650abf6306f76208dfaf8020833b1e4d
Johnny Hu <johnny@ciandt.com>
2019-12-09 14:58:41 -0800

    Try something different

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 /* eslint-disable */
-import { cd, chmod, exec, ln, mkdir } from 'shelljs';
 import * as core from '@actions/core';
 import * as github from '@actions/github';
+import danger from 'danger';
 import { DangerChecks } from './dangerfile';
 
 try {
@@ -9,22 +9,14 @@ try {
   const nameToGreet = core.getInput('who-to-greet');
   const manualMergeTag = core.getInput('manual-merge-label');
 
-  const path = '/usr/src/danger';
-  mkdir(path);
-  cd(path);
-  exec('git clone https://github.com/danger/danger-js.git .');
-  exec('yarn && yarn run build:fast');
-  chmod('+x', 'distribution/commands/danger.js');
-  ln('-s', '$(pwd)/distribution/commands/danger.js', path);
-
-  console.log(`Hello ${nameToGreet}! ---- ${manualMergeTag}`);
+  console.log(`Hello ${nameToGreet}! ---- ${manualMergeTag}`, danger);
   const time = new Date().toTimeString();
   core.setOutput('time', time);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2);
   console.log(`The event payload: ${payload}`);
 
-  // new DangerChecks({ manualMergeTag }).run();
+  new DangerChecks({ manualMergeTag }).run();
 } catch (error) {
   core.setFailed(error.message);
 }
