ca05bc55f6150a8019d7ab8030095852f4178929
Johnny Hu <johnny@ciandt.com>
2019-12-11 14:52:35 -0800

    More troubleshooting

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,12 +2,6 @@
 
 echo "Hello $1"
 
-# Install danger cli on the machine
-yarn global add danger
-
-# install needed dependencies
-yarn
-
 # Run danger
-danger --dangerfile "dangerfile.ts" ci
+danger ci --dangerfile $1
 
